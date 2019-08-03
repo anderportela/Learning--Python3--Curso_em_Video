1dcc877479628f0dbf32b712c4238bca67f45b55
Anderson Portela <44451495+anderportela@users.noreply.github.com>
2019-08-03 20:28:43 -0300

    Rename ex008.py to Conversor de Medidas.py

---
+++ Conversor de Medidas.py
@@ -0,0 +1,2 @@
+m = float(input('Digite um valor em metros: '))
+print('O valor em centímetros é: {:.0f}\nO valor em milímetros é: {:.0f}'.format(m*100, m*1000))
--- ex008.py
+++
@@ -1,2 +0,0 @@
-m = float(input('Digite um valor em metros: '))
-print('O valor em centímetros é: {:.0f}\nO valor em milímetros é: {:.0f}'.format(m*100, m*1000))
